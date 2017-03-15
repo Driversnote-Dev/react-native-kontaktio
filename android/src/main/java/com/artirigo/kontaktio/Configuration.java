47a257c2067dfee1d9902d1c04693d3462cfca1b
Andre Kovac <me@andrekovac.com>
2017-03-15 17:11:22 -0400

    Add further configurations and fix error with non-working activityCheckConfiguration

--- android/src/main/java/com/artirigo/kontaktio/Configuration.java
+++ android/src/main/java/com/artirigo/kontaktio/Configuration.java
@@ -43,37 +43,92 @@ class Configuration {
         try {
             // ScanMode (constant ranging or selective monitoring)
             if (params.hasKey("scanMode") && !params.isNull("scanMode")) {
-                scanMode = getScanMode(params.getInt("scanMode"), promise);
+                scanMode = getScanMode(params.getInt("scanMode"));
             }
 
             // ScanPeriod (active and passive scan periods)
             if (params.hasKey("scanPeriod") && !params.isNull("scanPeriod")) {
                 if (params.getType("scanPeriod") == ReadableType.String) {
-                    scanPeriod = getScanPeriod(params.getString("scanPeriod"), promise);
+                    scanPeriod = getScanPeriod(params.getString("scanPeriod"));
 
-                    // NB: ScanPeriod.create causes App crash if not in correct range
-                    // See https://github.com/Artirigo/react-native-kontaktio/issues/11
-//                } else if (params.getType("scanPeriod") == ReadableType.Map) {
-//                    scanPeriod = ScanPeriod.create(
-//                            params.getMap("scanPeriod").getInt("activePeriod"),
-//                            params.getMap("scanPeriod").getInt("passivePeriod")
-//                    );
+                } else if (params.getType("scanPeriod") == ReadableType.Map) {
+                    ReadableMap scanPeriodMap = params.getMap("scanPeriod");
 
+                    scanPeriod = ScanPeriod.create(
+                            scanPeriodMap.getInt("activePeriod"),
+                            scanPeriodMap.getInt("passivePeriod")
+                    );
                 } else {
-                    throw new Exception("scanPeriod has to be of type STRING");
+                    throw new Exception("scanPeriod has to be a STRING or an OBJECT of INTEGERS");
                 }
             }
 
             // ActivityCheckConfiguration (governs Beacon disappearance)
             if (params.hasKey("activityCheckConfiguration") && !params.isNull("activityCheckConfiguration")) {
-                getActivityCheckConfiguration(params.getString("activityCheckConfiguration"), promise);
+                if (params.getType("activityCheckConfiguration") == ReadableType.String) {
+                    activityCheckConfiguration = getActivityCheckConfiguration(params.getString("activityCheckConfiguration"));
+                } else if (params.getType("activityCheckConfiguration") == ReadableType.Map) {
+
+                    ReadableMap activityCheckConfigurationMap = params.getMap("activityCheckConfiguration");
+                    activityCheckConfiguration = ActivityCheckConfiguration.create(
+                            activityCheckConfigurationMap.getInt("inactivityTimeout"),
+                            activityCheckConfigurationMap.getInt("checkPeriod")
+                    );
+                } else {
+                    throw new Exception("activityCheckConfiguration has to be a STRING or an OBJECT of INTEGERS");
+                }
+            }
+
+            // After setting ScanPeriod and ActivityCheckConfiguration: Check for faulty configuration
+            // Active Scan Period must be bigger than checkPeriod for it to work as documented in
+            // https://github.com/Artirigo/react-native-kontaktio/issues/11
+            // ScanPeriod.create causes App crash if not in correct range
+            if (scanPeriod.getActivePeriod() < activityCheckConfiguration.getCheckPeriod()) {
+                throw new Exception("scanPeriod.activePeriod (" + scanPeriod.getActivePeriod() +
+                        ") has to be greater than activityCheckConfiguration.checkPeriod (" + activityCheckConfiguration.getCheckPeriod() + ").");
             }
 
             // ForceScanConfiguration (Concerns a bug on some Android devices)
+            if (params.hasKey("forceScanConfiguration") && !params.isNull("forceScanConfiguration")) {
+                if (params.getType("forceScanConfiguration") == ReadableType.String) {
+                    forceScanConfiguration = getForceScanConfiguration(params.getString("forceScanConfiguration"));
+                } else if (params.getType("forceScanConfiguration") == ReadableType.Map) {
+
+                    ReadableMap forceScanConfigurationMap = params.getMap("forceScanConfiguration");
+                    int activePeriod = forceScanConfigurationMap.getInt("forceScanActivePeriod");
+                    int passivePeriod = forceScanConfigurationMap.getInt("forceScanPassivePeriod");
+
+                    if (activePeriod < 1000) {
+                        throw new Exception("forceScanConfiguration.forceScanActivePeriod has to be greater equal 1000ms");
+                    }
+                    if (passivePeriod < 500) {
+                        throw new Exception("forceScanConfiguration.forceScanPassivePeriod has to be greater equal 500ms");
+                    }
+
+                    activityCheckConfiguration = ActivityCheckConfiguration.create(
+                            activePeriod,
+                            passivePeriod
+                    );
+                } else {
+                    throw new Exception("activityCheckConfiguration has to be a STRING or an OBJECT of INTEGERS");
+                }
+            }
 
             // monitoringEnabled (send data to Kontakt.io panel)
+            if (params.hasKey("monitoringEnabled") && !params.isNull("monitoringEnabled")) {
+                if (params.getType("monitoringEnabled") == ReadableType.Boolean) {
+                    if (!params.getBoolean("monitoringEnabled")) {
+                        monitoringEnabled = false;
+                    }
+                } else {
+                    throw new Exception("monitoringEnabled has to be of type BOOLEAN");
+                }
+            }
 
             // monitoringSyncInterval (how often should data be sent to Kontakt.io panel)
+            if (params.hasKey("monitoringSyncInterval") && !params.isNull("monitoringSyncInterval")) {
+                monitoringSyncInterval = params.getInt("monitoringSyncInterval");
+            }
 
 
             proximityManager.configuration()
@@ -96,7 +151,7 @@ class Configuration {
         }
     }
 
-    private ScanMode getScanMode(int mode, Promise promise) throws Exception {
+    private ScanMode getScanMode(int mode) throws Exception {
         switch (mode) {
             case 0:
                 return ScanMode.LOW_POWER;
@@ -110,7 +165,7 @@ class Configuration {
         }
     }
 
-    private ScanPeriod getScanPeriod(String mode, Promise promise) throws Exception {
+    private ScanPeriod getScanPeriod(String mode) throws Exception {
         switch (mode) {
             case "RANGING":
                 return ScanPeriod.RANGING;
@@ -122,7 +177,7 @@ class Configuration {
         }
     }
 
-    private ActivityCheckConfiguration getActivityCheckConfiguration(String mode, Promise promise) throws Exception {
+    private ActivityCheckConfiguration getActivityCheckConfiguration(String mode) throws Exception {
         switch (mode) {
             case "DISABLED":
                 return ActivityCheckConfiguration.DISABLED;
@@ -132,6 +187,18 @@ class Configuration {
                 return ActivityCheckConfiguration.DEFAULT;
             default:
                 throw new Exception("activityCheckConfiguration default value has to be either DISABLED, MINIMAL or DEFAULT");
+//                return ActivityCheckConfiguration.DEFAULT;
+        }
+    }
+
+    private ForceScanConfiguration getForceScanConfiguration(String mode) throws Exception {
+        switch (mode) {
+            case "DISABLED":
+                return ForceScanConfiguration.DISABLED;
+            case "MINIMAL":
+                return ForceScanConfiguration.MINIMAL;
+            default:
+                throw new Exception("forceScanConfiguration default value has to be either DISABLED or MINIMAL");
 //                return ActivityCheckConfiguration.DEFAULT;
         }
     }
