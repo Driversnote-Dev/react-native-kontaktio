9abe58150146b7d58c1d386a5527751ea4a693b6
Andre Kovac <me@andrekovac.com>
2017-04-10 08:38:35 +0200

    Return to single index.js file

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-import { NativeModules, NativeEventEmitter } from 'react-native';
+import {
+  NativeModules,
+  NativeEventEmitter,
+  Platform,
+} from 'react-native';
+
 import {
   IBEACON,
   EDDYSTONE,
@@ -10,87 +15,74 @@ import {
   monitoringSyncInterval,
 } from './configurations';
 
-// If the native module (i.e. Java module) is prefixed with "RCT", 
+// If the native module (i.e. Java module) is prefixed with "RCT",
 // the NativeModules name does not include "RCT".
 const KontaktModule = NativeModules.KontaktBeacons;
 
-const init = (apiKey = null, beaconTypes = null) => (
-  KontaktModule.init(apiKey, beaconTypes)
-);
-const initBeacons = KontaktModule.initBeacons;
-const configure = KontaktModule.configure;
-const startScanning = KontaktModule.startScanning;
-const stopScanning = KontaktModule.stopScanning;
-const restartScanning = KontaktModule.restartScanning;
-const isScanning = KontaktModule.isScanning;
-const disconnect = KontaktModule.disconnect;
-const isConnected = KontaktModule.isConnected;
-const setBeaconRegion = (region = null) => KontaktModule.setBeaconRegion(region);
-const setBeaconRegions = (regionsArray = null) => KontaktModule.setBeaconRegions(regionsArray);
+let Kontakt = {};
 
-const DEFAULT_KONTAKT_BEACON_PROXIMITY_UUID = KontaktModule.DEFAULT_KONTAKT_BEACON_PROXIMITY_UUID;
-const DEFAULT_KONTAKT_NAMESPACE_ID = KontaktModule.DEFAULT_KONTAKT_NAMESPACE_ID;
-const ANY_MINOR = KontaktModule.ANY_MINOR;
-const ANY_MAJOR = KontaktModule.ANY_MAJOR;
+if (Platform.OS === "android") {
+  const init = (apiKey = null, beaconTypes = null) => (
+    KontaktModule.init(apiKey, beaconTypes)
+  );
+  const configure = KontaktModule.configure;
+  const startScanning = KontaktModule.startScanning;
+  const stopScanning = KontaktModule.stopScanning;
+  const restartScanning = KontaktModule.restartScanning;
+  const isScanning = KontaktModule.isScanning;
+  const disconnect = KontaktModule.disconnect;
+  const isConnected = KontaktModule.isConnected;
+  const setBeaconRegion = (region = null) => KontaktModule.setBeaconRegion(region);
+  const setBeaconRegions = (regionsArray = null) => KontaktModule.setBeaconRegions(regionsArray);
 
-const ANDRE_CONSTANT = KontaktModule.ANDRE_CONSTANT;
+  const DEFAULT_KONTAKT_BEACON_PROXIMITY_UUID = KontaktModule.DEFAULT_KONTAKT_BEACON_PROXIMITY_UUID;
+  const DEFAULT_KONTAKT_NAMESPACE_ID = KontaktModule.DEFAULT_KONTAKT_NAMESPACE_ID;
+  const ANY_MINOR = KontaktModule.ANY_MINOR;
+  const ANY_MAJOR = KontaktModule.ANY_MAJOR;
 
-export default Kontakt = {
-  init,
-  KontaktModule,
-  initBeacons,
-  configure,
-  disconnect,
-  isConnected,
-  startScanning,
-  stopScanning,
-  restartScanning,
-  isScanning,
-  setBeaconRegion,
-  setBeaconRegions,
-  // Constants
-  IBEACON,
-  EDDYSTONE,
-  DEFAULT_KONTAKT_BEACON_PROXIMITY_UUID,
-  DEFAULT_KONTAKT_NAMESPACE_ID,
-  ANY_MAJOR,
-  ANY_MINOR,
-  ANDRE_CONSTANT,
-  // Configurations
-  scanMode,
-  scanPeriod,
-  activityCheckConfiguration,
-  forceScanConfiguration,
-  monitoringEnabled,
-  monitoringSyncInterval,
-};
+  const ANDRE_CONSTANT = KontaktModule.ANDRE_CONSTANT;
 
-export {
-  init,
-  KontaktModule,
-  initBeacons,
-  configure,
-  disconnect,
-  isConnected,
-  startScanning,
-  stopScanning,
-  restartScanning,
-  isScanning,
-  setBeaconRegion,
-  setBeaconRegions,
-  // Constants
-  IBEACON,
-  EDDYSTONE,
-  DEFAULT_KONTAKT_BEACON_PROXIMITY_UUID,
-  DEFAULT_KONTAKT_NAMESPACE_ID,
-  ANY_MAJOR,
-  ANY_MINOR,
-  ANDRE_CONSTANT,
-  // Configurations
-  scanMode,
-  scanPeriod,
-  activityCheckConfiguration,
-  forceScanConfiguration,
-  monitoringEnabled,
-  monitoringSyncInterval,
+  Kontakt = {
+    init,
+    KontaktModule,
+    configure,
+    disconnect,
+    isConnected,
+    startScanning,
+    stopScanning,
+    restartScanning,
+    isScanning,
+    setBeaconRegion,
+    setBeaconRegions,
+    // Constants
+    IBEACON,
+    EDDYSTONE,
+    DEFAULT_KONTAKT_BEACON_PROXIMITY_UUID,
+    DEFAULT_KONTAKT_NAMESPACE_ID,
+    ANY_MAJOR,
+    ANY_MINOR,
+    ANDRE_CONSTANT,
+    // Configurations
+    scanMode,
+    scanPeriod,
+    activityCheckConfiguration,
+    forceScanConfiguration,
+    monitoringEnabled,
+    monitoringSyncInterval,
+  };
 }
+
+if (Platform.OS === "ios") {
+
+  // const initBeacons = KontaktModule.initBeacons;
+  const test = KontaktModule.test;
+
+  const ANDRE_CONSTANT = KontaktModule.ANDRE_CONSTANT;
+
+  Kontakt = {
+    test,
+    ANDRE_CONSTANT,
+  };
+}
+
+export default Kontakt;
