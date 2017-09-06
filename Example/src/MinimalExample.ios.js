2e6946e7a6dcb2b2012f063838ed3d2a0283e9ac
Andre Kovac <me@andrekovac.com>
2017-09-06 15:21:38 +0200

    Fix wrong function name in MinimalExample for ios

--- Example/src/MinimalExample.ios.js
+++ Example/src/MinimalExample.ios.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { View, NativeEventEmitter } from 'react-native';
 
 import Kontakt, { KontaktModule } from 'react-native-kontaktio';
-const { init, startScanning } = Kontakt;
+const { init, startDiscovery } = Kontakt;
 
 const kontaktEmitter = new NativeEventEmitter(KontaktModule);
 
