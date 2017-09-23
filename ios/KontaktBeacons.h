a718c85a2d55da2372278dcd9ae8977fd53197c3
SAKAMOTO Kazunori <exkazuu@gmail.com>
2017-09-23 11:45:34 +0900

    Fix imports for React Native 0.48.x.

--- ios/KontaktBeacons.h
+++ ios/KontaktBeacons.h
@@ -1,13 +1,13 @@
-#if __has_include("RCTBridgeModule.h")
-  #import "RCTBridgeModule.h"
-#else
+#if __has_include(<React/RCTBridgeModule.h>)
   #import <React/RCTBridgeModule.h>
+#else
+  #import "RCTBridgeModule.h"
 #endif
 
-#if __has_include("RCTEventEmitter.h")
-  #import "RCTEventEmitter.h"
-#else
+#if __has_include(<React/RCTEventEmitter.h>)
   #import <React/RCTEventEmitter.h>
+#else
+  #import "RCTEventEmitter.h"
 #endif
 
 
