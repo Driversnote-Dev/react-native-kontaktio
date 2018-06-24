0e7adb8af4709088e6db5ceeb8266eaa557a69a9
Andre Kovac <me@andrekovac.com>
2018-06-24 23:22:34 +0200

    Add SECURE_PROFILE as constant and info about it to documentation

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-import {
-  NativeModules,
-  NativeEventEmitter,
-  Platform,
-} from 'react-native';
+import { NativeModules, NativeEventEmitter, Platform } from 'react-native';
 
 import {
   IBEACON,
   EDDYSTONE,
+  SECURE_PROFILE,
   scanMode,
   scanPeriod,
   activityCheckConfiguration,
@@ -22,27 +19,28 @@ export const KontaktModule = NativeModules.KontaktBeacons;
 /**
  * Methods shared in android and iOS
  */
-export const configure = (params = null) => (
-  KontaktModule.configure(params)
-);
-
+export const configure = (params = null) => KontaktModule.configure(params);
 
 let Kontakt = {};
 
 /**
  * Android
  */
-if (Platform.OS === "android") {
-  const connect = (apiKey = null, beaconTypes = null) => (
-    KontaktModule.connect(apiKey, beaconTypes)
-  );
+if (Platform.OS === 'android') {
+  const connect = (apiKey = null, beaconTypes = null) =>
+    KontaktModule.connect(
+      apiKey,
+      beaconTypes,
+    );
   /**
    * @deprecated
    * 'init' was renamed to 'connect' - please use 'connect'
    */
-  const init = (apiKey = null, beaconTypes = null) => (
-    KontaktModule.connect(apiKey, beaconTypes)
-  );
+  const init = (apiKey = null, beaconTypes = null) =>
+    KontaktModule.connect(
+      apiKey,
+      beaconTypes,
+    );
 
   const disconnect = KontaktModule.disconnect;
   const isConnected = KontaktModule.isConnected;
@@ -52,14 +50,19 @@ if (Platform.OS === "android") {
   const restartScanning = KontaktModule.restartScanning;
   const isScanning = KontaktModule.isScanning;
 
-  const setBeaconRegion = (region = null) => KontaktModule.setBeaconRegion(region);
-  const setBeaconRegions = (regionsArray = null) => KontaktModule.setBeaconRegions(regionsArray);
+  const setBeaconRegion = (region = null) =>
+    KontaktModule.setBeaconRegion(region);
+  const setBeaconRegions = (regionsArray = null) =>
+    KontaktModule.setBeaconRegions(regionsArray);
   const getBeaconRegions = KontaktModule.getBeaconRegions;
 
-  const setEddystoneNamespace = (namespace = null) => KontaktModule.setEddystoneNamespace(namespace);
+  const setEddystoneNamespace = (namespace = null) =>
+    KontaktModule.setEddystoneNamespace(namespace);
 
-  const DEFAULT_KONTAKT_BEACON_PROXIMITY_UUID = KontaktModule.DEFAULT_KONTAKT_BEACON_PROXIMITY_UUID;
-  const DEFAULT_KONTAKT_NAMESPACE_ID = KontaktModule.DEFAULT_KONTAKT_NAMESPACE_ID;
+  const DEFAULT_KONTAKT_BEACON_PROXIMITY_UUID =
+    KontaktModule.DEFAULT_KONTAKT_BEACON_PROXIMITY_UUID;
+  const DEFAULT_KONTAKT_NAMESPACE_ID =
+    KontaktModule.DEFAULT_KONTAKT_NAMESPACE_ID;
   const ANY_MINOR = KontaktModule.ANY_MINOR;
   const ANY_MAJOR = KontaktModule.ANY_MAJOR;
 
@@ -79,6 +82,7 @@ if (Platform.OS === "android") {
     // Constants
     IBEACON,
     EDDYSTONE,
+    SECURE_PROFILE,
     DEFAULT_KONTAKT_BEACON_PROXIMITY_UUID,
     DEFAULT_KONTAKT_NAMESPACE_ID,
     ANY_MAJOR,
@@ -90,32 +94,30 @@ if (Platform.OS === "android") {
     forceScanConfiguration,
     monitoringEnabled,
     monitoringSyncInterval,
-    init,   // @deprecated
+    init, // @deprecated
   };
 }
 
 /**
  * iOS
  */
-if (Platform.OS === "ios") {
-
-  const init = (apiKey = null) => (
-    KontaktModule.init(apiKey)
-  );
-  const startDiscovery = (interval = null) => (
-    KontaktModule.startDiscovery(interval)
-  );
+if (Platform.OS === 'ios') {
+  const init = (apiKey = null) => KontaktModule.init(apiKey);
+  const startDiscovery = (interval = null) =>
+    KontaktModule.startDiscovery(interval);
   const stopDiscovery = KontaktModule.stopDiscovery;
   const restartDiscovery = KontaktModule.restartDiscovery;
   const isDiscovering = KontaktModule.isDiscovering;
 
   const getAuthorizationStatus = KontaktModule.getAuthorizationStatus;
-  const requestWhenInUseAuthorization = KontaktModule.requestWhenInUseAuthorization;
+  const requestWhenInUseAuthorization =
+    KontaktModule.requestWhenInUseAuthorization;
   const requestAlwaysAuthorization = KontaktModule.requestAlwaysAuthorization;
 
   const startRangingBeaconsInRegion = KontaktModule.startRangingBeaconsInRegion;
   const stopRangingBeaconsInRegion = KontaktModule.stopRangingBeaconsInRegion;
-  const stopRangingBeaconsInAllRegions = KontaktModule.stopRangingBeaconsInAllRegions;
+  const stopRangingBeaconsInAllRegions =
+    KontaktModule.stopRangingBeaconsInAllRegions;
   const getRangedRegions = KontaktModule.getRangedRegions;
 
   const startMonitoringForRegion = KontaktModule.startMonitoringForRegion;
