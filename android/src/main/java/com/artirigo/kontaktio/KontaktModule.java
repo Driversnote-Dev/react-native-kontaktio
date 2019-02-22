3b0d5cba6e07d871dff8abd548f28161342abd79
André Kovac <me@andrekovac.com>
2019-02-22 18:48:34 +0100

    isConnected function: Make null check easier to fail.
    This should not change the behavior since if scanManager != null, proximityManager should also != null.
    But in a rare circumstance it might be that only one of them fails.

--- android/src/main/java/com/artirigo/kontaktio/KontaktModule.java
+++ android/src/main/java/com/artirigo/kontaktio/KontaktModule.java
@@ -148,7 +148,7 @@ public class KontaktModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void isConnected(Promise promise) {
-        if (beaconProximityManager != null && proximityManager != null) {
+        if (beaconProximityManager != null || proximityManager != null) {
             beaconProximityManager.isConnected(promise);
         } else {
             Log.w(Constants.TAG, "Did you forget to call connect() or did the connect() call fail? The beaconProximityManager object is not defined.");
